335b07304de1e5a841574b3063bfccb1f0a4d495
Christine Murray Graham <cmurrayjm@gmail.com>
2023-05-13 17:33:38 -0700

    first commit for question 1

---
+++ question1.js
@@ -0,0 +1,21 @@
+function isEven(num) {
+	return num % 2 === 0;
+}
+
+
+
+function filter(arr, callback) {
+	var newArr = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (!callback(arr[i])) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
+}
+
+let output = filter([1, 2, 3, 4], isEven); // [1, 3]
+console.log(output);
+
+let output2 = filter([1, 2, 3, 4, 5, 6, 7, 8], isEven); // [1, 3]
+console.log(output2);
